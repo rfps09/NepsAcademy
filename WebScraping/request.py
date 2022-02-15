ce985aa0a92604c0f1a1dd65bef9f9f4626eb870
rfps09 <rafael09felipe@gmail.com>
2022-02-15 12:50:34 -0300

    pandas

--- WebScraping/request.py
+++ WebScraping/request.py
@@ -1,12 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-x = requests.get("https://books.toscrape.com/")
+r = requests.get("https://books.toscrape.com")
 
-if x.status_code == 200:
-    h = BeautifulSoup(x.text,"html.parser")
+categories = []
+if r.status_code == 200:
+    page_content = r.text
+    soup = BeautifulSoup(page_content, "html.parser")
 
-    li_content = h.select(".side_categories ul li ul li")
+    categories_li = soup.select(".side_categories ul li ul li")
 
-    for li in li_content:
-        print(li.text.replace("\n", "").lstrip().rstrip())
+    for li in categories_li:
+        name = li.text.replace("\n", "").rstrip().lstrip()
+        link = li.find("a").get("href")
+
+        categories.append([name, link])
+
+    df = pd.DataFrame(data=categories, columns=["Name", "Link"])
+    df = df.sort_values(by="Name")
+
+    # Salvando os dados no arquivo categories.csv
+    df.to_csv("categories.csv")
+
+    # Para carregar os dados nós usamos o read_csv. O parâmetro index_col parameter informa o pandas que a primeira coluna do nosso csv está guardando o indice
+    df = pd.read_csv("categories.csv", index_col=0)
+    for index,book in df.iterrows():
+        print("{}: {} , {}".format(index,book["Name"], book["Link"]))
