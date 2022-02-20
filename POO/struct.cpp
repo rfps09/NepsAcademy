65ae784a9bbeafd848a0ca9d520126528eb1b534
rfps09 <rafael09felipe@gmail.com>
2022-02-20 11:43:08 -0300

    struct

--- POO/struct.cpp
+++ POO/struct.cpp
@@ -5,6 +5,7 @@ struct Inimigo {
     int x; 
     int y;
     bool vivo;
+    static int quantidade_vivos;
 
     //É necessário ter um construtor sem parâmetros para criar o vetor na função principal
     Inimigo(){
@@ -15,20 +16,29 @@ struct Inimigo {
     }
 
     //TODO: Crie um construtor que inicializa um inimigo usando os parâmetros abaixo.
-    Inimigo(int ID, int X, int Y, bool VIVO){  
-		id = ID;
-		x = X;
-		y = Y;
-		vivo = VIVO;
+    Inimigo(int ID, int X, int Y, int VIVO){        
+        id = ID;
+        x = X;
+        y = Y;
+        vivo = VIVO;
     }
 
-    //TODO: Método que muda a o status do inimigo de vivo para morto caso seja acertado pelo lazer na posição (X,Y).
+    //TODO: Método que muda a o status do inimigo de vivo para morto caso seja acertado pelo lazer na posição (X,Y). Também deve atualizar a variável quantidade_vivos.
     void foi_acertado(int X, int Y){
-		if(x == X && y == Y)
-			vivo = false;
+        if(x == X && y == Y && vivo) {
+            vivo = false;
+            quantidade_vivos--;
+        }
     }
 
+    static void qtd_vivos();
 };
+
+int Inimigo::quantidade_vivos = 0;
+
+void Inimigo::qtd_vivos() {
+    printf("qtd de vivos: %d\n", Inimigo::quantidade_vivos);
+}
 
 int main(){
 
@@ -36,6 +46,7 @@ int main(){
     scanf("%d", &N); 
 
     Inimigo inimigo[N];
+    Inimigo::quantidade_vivos = N;
 
     for(int id=0;id<N;id++){
         int x, y;
@@ -45,7 +56,7 @@ int main(){
     }
 
     int M; //Quantidade de Inimigos
-    scanf("%d", &M); 
+    scanf("%d", &M);     
 
     for(int i=0;i<M;i++){
         int x, y;
@@ -56,9 +67,6 @@ int main(){
         }
     }
 
-    //TODO: Crie um laço for que imprime o id de todos os inimigos que foram acertados.
-	for(int id=0;id<N;id++){
-		if(!inimigo[id].vivo)
-			printf("%d ", inimigo[id].id);
-	} printf("\n");
+    printf("vivos: %d\n", Inimigo::quantidade_vivos);
+    printf("mortos: %d\n", N-Inimigo::quantidade_vivos);
 }
